1b9335cfe3d1f660ecee9c568907b46f45cf91c2
Michael Studley <michael.r.studley@gmail.com>
2024-05-07 18:37:12 +0700

    removed the cached_property decorator from the full_list method in utils.py to allow me to override the methods in BaseManager for certain classes and get the desired results.

--- data/item_info.py
+++ data/item_info.py
@@ -21,29 +21,6 @@ class ItemInfos(BaseManager):
         super().__init__('item_info', ItemInfo)
 
 
-# print(ItemInfos().full_list)
-# print(ItemInfos().search(id=1))
-
-
-# class ItemInfos:
-#     @cached_property
-#     def full_list(self) -> list[ItemInfo]:
-#         query = "SELECT * FROM `item_info`"
-#         all_rows = db.fetchall(query)
-#         return [ItemInfo(**row) for row in all_rows]
-#
-#     @cache
-#     def search(self, **kwargs) -> list[ItemInfo] | list[str]:
-#         results = self.full_list[:]
-#         for item_info in self.full_list:
-#             for key in kwargs:
-#                 try:
-#                     value = getattr(item_info, key)
-#                 except AttributeError:
-#                     return [f"Key '{key}' does not exist..."]
-#
-#                 if value != kwargs[key]:
-#                     results.remove(item_info)
-#                     break
-#
-#         return results
+item_infos = ItemInfos()
+print(item_infos.full_list())
+print(item_infos.search(id=1))
