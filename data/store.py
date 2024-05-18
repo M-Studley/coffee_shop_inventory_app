43e602401afdde267e9dc53f9ddeb58665232a34
Michael Studley <michael.r.studley@gmail.com>
2024-05-18 12:21:29 +0700

    changed the update to the utils.py for a more centralized approach and added a wrapper for the singular class to keep using the namespace of the dataclasses along with a return of the changes for validation.
    Not sure what the best option would be.  copy the original search method to each data file or centralize it and copy the wrapper to all data files.

--- data/store.py
+++ data/store.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from data.location import Locations
 from data.inventory import Inventories
 from data.employee import Employees
-from data.utils import Searchable
+from data.utils import Searchable, update_instance
 from database.database import Database as db
 
 
@@ -26,14 +26,23 @@ class Store:
         return Employees().search(_store_number_id=self.id)
 
     def update(self, **kwargs):
-        set_statement = ', '.join([f"`{key}` = '{value}'" for key, value in kwargs.items()])
-        query = f"""
-        UPDATE `{self.__class__.__name__.lower()}`
-        SET {set_statement}
-        WHERE `id` = %s;
-        """
-        values = (self.id,)
-        db.execute(query, values)
+        update_instance(self, **kwargs)
+
+        updated_instances = Stores().search(id=self.id)
+        if updated_instances:
+            return next(iter(updated_instances))
+        else:
+            raise Exception("Updated instance not found...")
+
+    # def update(self, **kwargs):
+    #     set_statement = ', '.join([f"`{key}` = '{value}'" for key, value in kwargs.items()])
+    #     query = f"""
+    #     UPDATE `{self.__class__.__name__.lower()}`
+    #     SET {set_statement}
+    #     WHERE `id` = %s;
+    #     """
+    #     values = (self.id,)
+    #     db.execute(query, values)
 
 
 class Stores(Searchable):
@@ -42,6 +51,5 @@ class Stores(Searchable):
 
 my_store = next(iter(Stores().search(id=2)))
 print(my_store)
-my_store.update(name='peets coffee', phone="111-222-3333")
-changed = next(iter(Stores().search(id=2)))
-print(changed)
+updated_store = my_store.update(name='peets coffee', phone='222-888-2288')
+print(updated_store)
--- data/utils.py
+++ data/utils.py
@@ -1,5 +1,16 @@
 import re
-from database.database import Database as db
+from database.database import Database as dB
+
+
+def update_instance(instance, **kwargs):
+    set_statement = ', '.join([f"`{key}` = %s" for key in kwargs.keys()])
+    query = f"""
+    UPDATE `{instance.__class__.__name__.lower()}`
+    SET {set_statement}
+    WHERE `id` = %s;
+    """
+    values = tuple(kwargs.values()) + (instance.id,)
+    dB.execute(query, values)
 
 
 class Searchable:
@@ -13,12 +24,12 @@ class Searchable:
     def search(self, **kwargs):
         table_name = self.camel_to_snake_case
         query = f"SELECT * FROM `{table_name}`"
-        all_rows = db.fetchall(query)
+        all_rows = dB.fetchall(query)
         results = [self.child(**row) for row in all_rows] # noqa
 
         for item in results[:]:
             for key in kwargs:
-                if not hasattr(item, key): raise Exception(f"Key '{key}' does not exist...")
+                if not hasattr(item, key): raise Exception(f"Key '{key}' does not exist...") # noqa
 
                 value = getattr(item, key)
 
